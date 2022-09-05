deab2e1c60580f9282e26bc724b045ab3e56ae98
Marcelo Navarro <marcelo.nd.mx@gmail.com>
2022-09-05 12:39:20 -0500

    Finish setting up metadata DF.
    Extracted list of samples in required order.

--- code/coding_task.R
+++ code/coding_task.R
@@ -1,30 +1,30 @@
-library(readr)
 library(dplyr)
 
-metadata <- read_csv("C:/Users/marce/Desktop/Metadata.csv", col_names = FALSE)
-
-metadata <- t(metadata)
-
-colnames(metadata) <- metadata[1,]
-
-metadata <- metadata[2:(nrow(metadata)-10),]
-
-rownames(metadata) <- metadata[,1]
-
-metadata <- data.frame(metadata[,-1])
-
+setwd("C:/Users/marce/Documents/Repos/coding_task/data")
 
 getDataFromTable <- function(data_path, vars_toDrop_beg = 0, vars_toDrop_end = 0){
-  dataTable <- readr::read_csv(data_path, col_names = FALSE)
+  dataTable <- readr::read_csv(data_path, col_names = FALSE, )
   dataTable <- t(dataTable)
   colnames(dataTable) <- dataTable[1,]
+  # Dropping rows that are not used, including the column where colnames where
   dataTable <- dataTable[(2+vars_toDrop_beg):(nrow(dataTable)-vars_toDrop_end),]
-  rownames(dataTable) <- dataTable[,1]
-  return(data.frame(dataTable[,-1]))
+  dataTable <- data.frame(dataTable, row.names = NULL)
+  # Return df with correct rownames and removing column where rownames where
+  return(tibble::column_to_rownames(dataTable, colnames(dataTable)[1]))
 }
 
-meta <- getDataFromTable("C:/Users/marce/Desktop/Metadata.csv", vars_toDrop_beg = 0, vars_toDrop_end = 10)
+# Reading and setting up metadata df 
+metadata <- getDataFromTable("Metadata.csv", vars_toDrop_beg = 0, vars_toDrop_end = 10)
 
-metadata_surface <- metadata %>% 
+metadata <-transform(metadata, ATTRIBUTE_Depth = as.numeric(ATTRIBUTE_Depth))
+
+metadata_surface <- metadata %>%
+  transform(ATTRIBUTE_Depth = as.numeric(ATTRIBUTE_Depth)) %>%
   filter(ATTRIBUTE_Depth_Range == "0-30") %>%
-  arrange(ATTRIBUTE_Location)
+  arrange(ATTRIBUTE_Location, ATTRIBUTE_Depth)
+
+# Extracting list of samples in order of cycle and day.
+samples_in_order <- rownames(metadata_surface)
+samples_in_order
+
+
