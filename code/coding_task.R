5bbd9a85b1fbef332cd524b87ddd7b18c592d6c3
Marcelo Navarro <marcelo.nd.mx@gmail.com>
2022-09-05 20:13:41 -0500

    Rarefactored code to make it simpler to read

--- code/coding_task.R
+++ code/coding_task.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(vegan)
 library(ggplot2)
 
+# Set working directory
 setwd("C:/Users/marce/Documents/Repos/coding_task/data")
 
 getDataFromTable <- function(data_path, vars_toDrop_beg = 0, vars_toDrop_end = 0){
@@ -15,48 +16,55 @@ getDataFromTable <- function(data_path, vars_toDrop_beg = 0, vars_toDrop_end = 0
   return(tibble::column_to_rownames(dataTable, colnames(dataTable)[1]))
 }
 
-# Reading and setting up metadata df 
+##### Read and set up metadata DataFrame
 metadata <- getDataFromTable("Metadata.csv", vars_toDrop_beg = 0, vars_toDrop_end = 10)
 
 metadata <-transform(metadata, ATTRIBUTE_Depth = as.numeric(ATTRIBUTE_Depth))
+
+metadata <- mutate(metadata, sample_name = paste(ATTRIBUTE_Location, ATTRIBUTE_Depth, sep = "_"))
 
 metadata_surface <- metadata %>%
   transform(ATTRIBUTE_Depth = as.numeric(ATTRIBUTE_Depth)) %>%
   filter(ATTRIBUTE_Depth_Range == "0-30") %>%
   arrange(ATTRIBUTE_Location, ATTRIBUTE_Depth)
 
-# Extracting list of samples in order of cycle and day.
-surface_samples_in_order <- rownames(metadata_surface)
-surface_samples_in_order
-
-# Reading all data
+##### Read ASVs data
+# Load 16s AVSs data
 asv_16s <- getDataFromTable("ASV_16S.csv", vars_toDrop_beg = 0, vars_toDrop_end = 8)
 
-#Converting char variables into numeric without loosing DF structure
+# Convert char variables into numeric without loosing DF structure
 for (colX in colnames(asv_16s)) {
-  print(colX)
   asv_16s[colX] <- as.numeric(asv_16s[colX][,1])
 }
 
-# Extracting surface samples in cycle-day order
-surface_asv16s <- asv_16s[surface_samples_in_order,]
+# Extract surface samples in cycle-day order
+surface_asv16s <- asv_16s[row.names(metadata_surface),]
 
+# Calculate alpha diversity indices for surface samples
+alpha_diversity_indices <- vegan::estimateR(surface_asv16s)
 
-data_richness <- estimateR(surface_asv16s)
+# I will only use chao1 for this exercise
+surface_asv16s_chao1 <- as.numeric(data.frame(t(alpha_diversity_indices))$S.chao1)
 
-t(data_richness)
+# Create DF with necessary data to plot
+surface_alpha_diversity <- bind_cols(select(metadata_surface, ATTRIBUTE_Location, sample_name), surface_asv16s_chao1)
+colnames(surface_alpha_diversity) <- c("ATTRIBUTE_Location", "sample_name", "Chao1")
 
-data.frame(t(data_richness))$S.chao1
+# Plot all samples separately
+ggplot(surface_alpha_diversity, aes(x=sample_name, y=Chao1))+
+  geom_point() +
+  labs(x = "Samples",
+       title = "Alpha diversity for 16S AVSs of Surface Samples") +
+  theme(title = element_text(size = 10),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
-test2 <- data.frame(cbind(row.names(surface_asv16s), as.numeric(data.frame(t(data_richness))$S.chao1)))
-
-test2[,2] <- as.numeric(test2[,2])
-
-ggplot(test2, aes(x=X1, y=X2))+
-  geom_point()
-
-
-
+# Plot average alpha diversity per cycle-day
+ggplot(surface_alpha_diversity, aes(x=ATTRIBUTE_Location, y=Chao1))+
+  geom_boxplot() +
+  labs(x = "Samples",
+       title = "Average Alpha diversity per cycle-day for 16S AVSs of Surface Samples") +
+  theme(title = element_text(size = 8),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 
 
